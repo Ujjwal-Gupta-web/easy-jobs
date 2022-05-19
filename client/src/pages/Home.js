6928124f2f57b6e158621dc4372bb9c61e9cf957
unknown <ujjgpta9@gmail.com>
2022-05-19 19:27:55 +0530

    made some change

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -21,17 +21,24 @@ export default function Home() {
   return (
     <>
       <div className="home-page no-scrollbar">
-        <div className="upperbar bg-indigo-600">
-          <div className="nav float-right p-[3rem] text-2xl font-encode text-white">
-            <Navbar />
-          </div>
+
+
+        <div className="upperbar bg-indigo-600 flex items-center justify-between flex-wrap">
+          <div>
           <Link to="/">
-            <h1 className="text-6xl text-white  shadow-2xl font-medium p-8 font-titan">
+            <h1 className="text-6xl text-white shadow-xl font-medium p-8 font-titan">
               {" "}
               Easy Jobs
             </h1>
           </Link>
+          </div>
+
+          <div className="nav p-[3rem] text-2xl font-encode text-white">
+            <Navbar />
+          </div>
         </div>
+
+        
 
         <div className="hero p-20 flex mb-48 flex-row">
           <div className="title p-5 ml-10 b">
